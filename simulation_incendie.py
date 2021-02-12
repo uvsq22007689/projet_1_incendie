6a94cbf7f2925adffc1003db7e0ac47782d104d3
Nabila ELAIDOUDI <nabila.elaidoudi@ens.uvsq.fr>
2021-02-12 11:57:21 +0100

    nv

---
+++ simulation_incendie.py
@@ -0,0 +1,31 @@
+########################################################
+# Auteurs:
+# Nabila Elaidoudi
+# Marco Laurent De-Riemacker
+# Gabriela Da Luz
+# Abdul-Malik 
+# Groupe: BI 1
+#https://github.com/uvsq22007689/projet_1_incendie.git
+##########################################################
+
+#####################
+#import des modules
+
+import tkinter as tk 
+import random as rd
+#####################
+# fonctions
+
+
+#####################
+# programme principal 
+racine = tk.Tk()
+COTE = 500
+racine.title("Incendie")
+canvas = tk.Canvas(racine,width = COTE, height = COTE, bg = "black")
+canvas.grid()
+
+
+
+
+racine.mainloop()
