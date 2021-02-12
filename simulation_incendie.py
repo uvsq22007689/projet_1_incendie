b089724c1360e330b5c5b21606ad0417acd5dd84
Marco <marc-antoine.laurent-de-riemacker@ens.uvsq.fr>
2021-02-12 12:12:11 +0100

    1.2

--- simulation_incendie.py
+++ simulation_incendie.py
@@ -4,19 +4,27 @@
 # Marco Laurent De-Riemacker
 # Gabriela Da Luz
 # Abdul-Malik 
-Groupe: BI 1
+# Groupe: BI 1
 #https://github.com/uvsq22007689/projet_1_incendie.git
 ##########################################################
 
 #####################
 #import des modules
 
-import tkinter as tk 
 
+
+import tkinter as tk 
+import random as rd
 #####################
 # fonctions
 
 
 #####################
 # programme principal 
+racine = tk.Tk()
+COTE = 500
+racine.title("Incendie")
+canvas = tk.Canvas(racine,width = COTE, height = COTE, bg = "black")
+canvas.grid()
 
+racine.mainloop()
